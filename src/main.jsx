3e980609e9f6dc46b78bc956394e5fb891cea07a
Sergei <chechet@mail.ru>
2026-05-05 15:55:22 +0400

    fix: apply global layout and center app content

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '@/app/styles/global.css'
 import { Provider } from 'react-redux'
 import { store } from '@/app/providers/store/store'
 import QuizPage from "./pages/quiz-page/ui/QuizPage.jsx";
